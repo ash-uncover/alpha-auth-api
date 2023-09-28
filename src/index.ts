f323774b334da7f2c2e9acea5feb95beab35604d
ash-uncover <ash.uncover@gmail.com>
2023-09-28 12:08:12 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,9 @@ import rest from './rest'
 import Logger, {
   LogConfig
 } from '@uncover/js-utils-logger'
-import ServerlessHttp from 'serverless-http'
 
 LogConfig.info()
 const LOGGER = new Logger('SERVER')
-
-let serverlessWrapper
 
 mongoose.connect(CONFIG.ALPHA_AUTH_DATABASE_CONN)
   .then(() => {
@@ -19,7 +16,6 @@ mongoose.connect(CONFIG.ALPHA_AUTH_DATABASE_CONN)
     const server = rest.listen(CONFIG.ALPHA_AUTH_REST_PORT, () => {
       LOGGER.info(`Server is running in ${CONFIG.ALPHA_AUTH_REST_PROTOCOL}://${CONFIG.ALPHA_AUTH_REST_HOST}:${CONFIG.ALPHA_AUTH_REST_PORT}`)
     })
-    serverlessWrapper = ServerlessHttp(rest)
     server.on('close', () => {
       LOGGER.debug('Server Shutting down')
       mongoose.connection.close()
@@ -29,8 +25,3 @@ mongoose.connect(CONFIG.ALPHA_AUTH_DATABASE_CONN)
     LOGGER.error('Failed to connect Mongo')
     LOGGER.error(error)
   })
-
-module.exports.handler = async (event, context) => {
-  const result = await serverlessWrapper(event, context)
-  return result
-}
